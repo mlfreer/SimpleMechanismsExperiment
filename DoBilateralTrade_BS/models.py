f88c41713912275d1f6ad5e36e8de7a52c3a91ef
mlfreer <1991F@inbox.ru>
2020-05-15 16:48:12 +0300

    Settinng up dominant strategy bilateral trade treatment

---
+++ DoBilateralTrade_BS/models.py
@@ -0,0 +1,35 @@
+from otree.api import (
+    models,
+    widgets,
+    BaseConstants,
+    BaseSubsession,
+    BaseGroup,
+    BasePlayer,
+    Currency as c,
+    currency_range,
+)
+
+
+author = 'Mikhail Freer'
+
+doc = """
+The treatment contains the Dominant Strategy Bilateral Trade Treatment
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'DoBilateralTrade_BS'
+    players_per_group = None
+    num_rounds = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    pass
