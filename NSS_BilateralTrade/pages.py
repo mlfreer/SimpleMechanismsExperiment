a0cb54f2b1b99b1254d5674caa62af731ad87f63
mlfreer <1991F@inbox.ru>
2020-05-18 19:30:17 +0300

    finishing NSS treatment

--- NSS_BilateralTrade/pages.py
+++ NSS_BilateralTrade/pages.py
@@ -3,16 +3,31 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class MyPage(Page):
-    pass
+class WelcomePage(Page):
+	template_name ='NSS_BilateralTrade/WelcomePage.html'
+	def is_displayed(self):
+		return self.player.subsession.round_number == 1
+
+	#after_all_players_arrive = 'player.set_value'
+
+class PriceInputPage(Page):
+	template_name ='NSS_BilateralTrade/PriceInputPage.html'
+	# always displayed
+	def is_displayed(self):
+		return True
+
+	form_model = 'player'
+	form_fields = ['personal_price']
 
 
 class ResultsWaitPage(WaitPage):
-    pass
+	after_all_players_arrive = 'set_payoffs'
+    #def after_all_players_arrive(self):
+    #	self.player.group.set_payoffs()
 
 
 class Results(Page):
-    pass
+    template_name ='NSS_BilateralTrade/Results.html'
 
 
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [WelcomePage, PriceInputPage, ResultsWaitPage, Results]
