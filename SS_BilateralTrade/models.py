d30c64862fb4e2e908b050bc18f3c3a4246f4c5b
mlfreer <1991F@inbox.ru>
2020-05-18 19:22:50 +0300

    finishing the SS treatment

--- SS_BilateralTrade/models.py
+++ SS_BilateralTrade/models.py
@@ -20,7 +20,7 @@ The treatment contains the Dominant Strategy Bilateral Trade Treatment
 
 
 class Constants(BaseConstants):
-	name_in_url = 'DominantStrategyBilateralTrade'
+	name_in_url = 'SimpleStrategyBilateralTrade'
 	players_per_group = 2
 	num_rounds = 1
 	# creating bounds for the support of the distribution of value:
@@ -35,8 +35,8 @@ class Subsession(BaseSubsession):
 		#self.group_randomly(fixed_id_in_group=True)
 
 		# setting the random prices:
-		for g in self.get_groups():
-			g.set_random_price()
+		#for g in self.get_groups():
+		#	g.set_random_price()
 
 		#print('setting the value')
 		# setting the values and the roles
@@ -47,16 +47,14 @@ class Subsession(BaseSubsession):
 
 class Group(BaseGroup):
 	# in DS Bilateral Trade the price is determined as random:
-	random_price = models.DecimalField(max_digits=5, decimal_places=1, default=0)
-	# method to support the random price:
-	def set_random_price(self):
-		# instead of using random.randrange that is for integers only, we use rand.uniform to have the float
-		self.random_price = random.uniform(Constants.min_support,Constants.max_support)
+	posted_price = models.DecimalField(max_digits=5, decimal_places=1, default=0)
+	
 
 	# setting payoffs function:
 	def set_payoffs(self):
 		p_buyer  = self.get_player_by_role('buyer')
 		p_seller = self.get_player_by_role('seller')
+		self.posted_price = p_seller.personal_price
 		if p_buyer.personal_price >= p_seller.personal_price:
 			p_buyer.profit = p_buyer.value - p_seller.personal_price
 			p_seller.profit = p_seller.personal_price - p_seller.value
