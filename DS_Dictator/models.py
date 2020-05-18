5ffdc4aff1d746e2e7c20af74917c6cd9e6b236e
mlfreer <1991F@inbox.ru>
2020-05-18 20:13:55 +0300

    defining the preference profiles

--- DS_Dictator/models.py
+++ DS_Dictator/models.py
@@ -1,12 +1,12 @@
 from otree.api import (
-    models,
-    widgets,
-    BaseConstants,
-    BaseSubsession,
-    BaseGroup,
-    BasePlayer,
-    Currency as c,
-    currency_range,
+	models,
+	widgets,
+	BaseConstants,
+	BaseSubsession,
+	BaseGroup,
+	BasePlayer,
+	Currency as c,
+	currency_range,
 )
 
 import random
@@ -19,37 +19,61 @@ Dominant Strategy Dictator Treatment
 
 
 class Constants(BaseConstants):
-    name_in_url = 'DS_Dictator'
-    players_per_group = 3
-    num_rounds = 1
+	name_in_url = 'DS_Dictator'
+	players_per_group = 3
+	num_rounds = 1
 
-    # payoffs from the more and less preferred alternatives:
-    high_payoff = 20
-    low_payoff = 10
+	# payoffs from the more and less preferred alternatives:
+	high_payoff = 20
+	low_payoff = 10
 
-    # higher prob of the type profile
-    prob_high = .9
-    # moderate probability of the choice profile
-    prob_mod = .5
+	# higher prob of the type profile
+	prob_high = .9
+	# moderate probability of the choice profile
+	prob_moderate = .5
 
 
 class Subsession(BaseSubsession):
-    pass
+
+	def creating_session(self):
+		# making sure that people are regroupped accordingly 
+		self.group_randomly(fixed_id_in_group=True)
 
 
 class Group(BaseGroup):
-    pass
+	CollectiveChoice = models.CharField(choices = ['A','B'])
 
 
 class Player(BasePlayer):
-    def role(self):
-        if self.id_in_group == 1:
-            return 1
-        if self.id_in_group == 2:
-            return 2
-        if self.id_in_group == 3:
-            return 3
+	preference_profile = models.CharField(choices = ['AB','BA'])
 
+	def role(self):
+		if self.id_in_group == 1:
+			return 1
+		if self.id_in_group == 2:
+			return 2
+		if self.id_in_group == 3:
+			return 3
+
+	def set_preference_profile(self):
+		if self.role == 1:
+			r = random.uniform(0,1)
+			if r > Constants.prob_high:
+				preference_profile = 'BA'
+			else:
+				preference_profile = 'AB'
+		if self.role == 2:
+			r = random.uniform(0,1)
+			if r > Constants.prob_moderate:
+				preference_profile = 'BA'
+			else:
+				preference_profile = 'AB'
+		if self.role == 3:
+			r = random.uniform(0,1)
+			if r > Constants.prob_high:
+				preference_profile = 'AB'
+			else:
+				preference_profile = 'BA'
 
 
 
