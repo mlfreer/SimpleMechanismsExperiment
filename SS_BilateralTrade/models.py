9c95c0661867a33cbee4684731901e06538e8f47
mlfreer <1991F@inbox.ru>
2020-05-18 19:31:25 +0300

    unifying the notation

--- SS_BilateralTrade/models.py
+++ SS_BilateralTrade/models.py
@@ -47,14 +47,14 @@ class Subsession(BaseSubsession):
 
 class Group(BaseGroup):
 	# in DS Bilateral Trade the price is determined as random:
-	posted_price = models.DecimalField(max_digits=5, decimal_places=1, default=0)
+	final_price = models.DecimalField(max_digits=5, decimal_places=1, default=0)
 	
 
 	# setting payoffs function:
 	def set_payoffs(self):
 		p_buyer  = self.get_player_by_role('buyer')
 		p_seller = self.get_player_by_role('seller')
-		self.posted_price = p_seller.personal_price
+		self.final_price = p_seller.personal_price
 		if p_buyer.personal_price >= p_seller.personal_price:
 			p_buyer.profit = p_buyer.value - p_seller.personal_price
 			p_seller.profit = p_seller.personal_price - p_seller.value
