90061827ac70414bccdaf7c67186dbb0ae32b554
mlfreer <1991F@inbox.ru>
2022-03-25 01:16:04 +0000

    updated quiz

--- quiz/models.py
+++ quiz/models.py
@@ -22,55 +22,29 @@ class Constants(BaseConstants):
     players_per_group = None
     num_rounds = 1
 
-    # Treatments
-    DominantStrategy = 1
-    FirstOrder = 2
-    SecondOrder = 3
-
     # quiz answers separated in blocks:
 
     # block 1: preferences, exampe 1
-    a11 = 4
-    a12 = 5
-    a13 = 2
-    a14 = 4
-    a15 = 4
-
-    # block 2: preferences example 2
-    a21 = 2
-    a22 = 5
-    a23 = 2
-    a24 = 5
-    a25 = 5
-
-    # block 3: Stage 1 voting (SOB)
-    a31 = 2
-    a32 = 5
-    a33 = 1
-    a34 = 5
-
-    # block 4: Stage 2 voting (SOB) or Stage 1 voting (FOB)
-    a41 = 2
-    a42 = 5
-    a43 = 1
-
-    # block 5: Stage 3 voting (SOB) or Stage 2 voting (FOB) or Stage 1 voting (DS)
-    a51 = 2
-    a52 = 5
+    a11 = 3
+    a12 = 2
+    a13 = 3
+    a14 = 3
+    a15 = 3
+    a21 = 7
 
         # defining the vector of preferences:
     preferences = [[0 for x in range(0,6)] for x in range(0,6)]
 
 
-    # type (4):
-    preferences[0] = [15, 5, 0, 20] # player 1
-    preferences[1] = [20, 15, 0, 5] # player 2a
-    preferences[2] = [20, 5, 0, 15] # player 2b
-    preferences[3] = [20, 15, 0, 5] # player 3a
-    preferences[4] = [20, 5, 0, 15] # player 3b
-    preferences[5] = [15, 20, 0, 5] # player 4
+# type (5):
+    preferences[0] = [20, 5, 15, 2] # player 1
+    preferences[1] = [15, 5, 20, 2] # player 2a
+    preferences[2] = [2, 15, 20, 5] # player 2b
+    preferences[3] = [15, 5, 20, 2] # player 3a
+    preferences[4] = [2, 15, 20, 5] # player 3b
+    preferences[5] = [2, 15, 5, 20] # player 4
 
-    alternatives = ['blue', 'green', 'purple', 'orange']
+    alternatives = ['blue', 'green', 'orange', 'purple',]
 
 
 class Subsession(BaseSubsession):
--- quiz/pages.py
+++ quiz/pages.py
@@ -76,16 +76,19 @@ class Question3(Page):
     def error_message(player, values):
         if values['question']!=Constants.a13:
             return 'error'
+#-----------------------------------------------------------------------------------
 
+
+#-----------------------------------------------------------------------------------
 class Question4(Page):
     form_model  = 'player'
     form_fields = ['question']
 
     def vars_for_template(self):
         return dict(
-            my_number = 1,
-            my_profile = 0,
-            my_preferences = Constants.preferences[0],
+            my_number = 2,
+            my_profile = 2,
+            my_preferences = Constants.preferences[2],
             preference_profiles = Constants.preferences
             )
 
@@ -105,9 +108,9 @@ class Question5(Page):
 
     def vars_for_template(self):
         return dict(
-            my_number = 1,
-            my_profile = 0,
-            my_preferences = Constants.preferences[0],
+            my_number = 2,
+            my_profile = 2,
+            my_preferences = Constants.preferences[2],
             preference_profiles = Constants.preferences
             )
 
@@ -119,13 +122,7 @@ class Question5(Page):
     def error_message(player, values):
         if values['question']!=Constants.a14:
             return 'error'
-#-----------------------------------------------------------------------------------
 
-
-
-
-#-----------------------------------------------------------------------------------
-# BLOCK 2 [second table]:
 class Question6(Page):
     form_model  = 'player'
     form_fields = ['question']
@@ -147,309 +144,6 @@ class Question6(Page):
         if values['question']!=Constants.a21:
             return 'error'
 
-class Question7(Page):
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        return dict(
-            my_number = 2,
-            my_profile = 2,
-            my_preferences = Constants.preferences[2],
-            preference_profiles = Constants.preferences
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a22:
-            return 'error'
-
-
-class Question8(Page):
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        return dict(
-            my_number = 2,
-            my_profile = 2,
-            my_preferences = Constants.preferences[2],
-            preference_profiles = Constants.preferences
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a23:
-            return 'error'
-
-class Question9(Page):
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        return dict(
-            my_number = 2,
-            my_profile = 2,
-            my_preferences = Constants.preferences[2],
-            preference_profiles = Constants.preferences
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a24:
-            return 'error'
-
-class Question10(Page):
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        return dict(
-            my_number = 2,
-            my_profile = 2,
-            my_preferences = Constants.preferences[2],
-            preference_profiles = Constants.preferences
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a25:
-            return 'error'
-#-----------------------------------------------------------------------------------
-
-
-
-
-#-----------------------------------------------------------------------------------
-# BLOCK 3 [SOB STAGE 1]:
-class Question31(Page):
-    def is_displayed(self):
-        return self.session.config['treatment'] >= Constants.SecondOrder
-
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        temp = self.session.config['treatment']-2
-        return dict(
-            stage_number = temp,
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a31:
-            return 'error'
-
-class Question32(Page):
-    def is_displayed(self):
-        return self.session.config['treatment'] >= Constants.SecondOrder
-
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        temp = self.session.config['treatment']-2
-        return dict(
-            stage_number = temp,
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a32:
-            return 'error'
-
-class Question33(Page):
-    def is_displayed(self):
-        return self.session.config['treatment'] >= Constants.SecondOrder
-
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        temp = self.session.config['treatment']-2
-        return dict(
-            stage_number = temp,
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a33:
-            return 'error'
-
-class Question34(Page):
-    def is_displayed(self):
-        return self.session.config['treatment'] >= Constants.SecondOrder
-
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        temp = self.session.config['treatment']-2
-        return dict(
-            stage_number = temp,
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a34:
-            return 'error'
-#-----------------------------------------------------------------------------------
-
-
-
-#-----------------------------------------------------------------------------------
-# BLOCK 4 [SOB STAGE 2]:
-class Question41(Page):
-    def is_displayed(self):
-        return self.session.config['treatment'] >= Constants.FirstOrder
-
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        temp = self.session.config['treatment']-1
-        return dict(
-            stage_number = temp,
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a41:
-            return 'error'
-
-class Question42(Page):
-    def is_displayed(self):
-        return self.session.config['treatment'] >= Constants.FirstOrder
-
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        temp = self.session.config['treatment']-1
-        return dict(
-            stage_number = temp,
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a42:
-            return 'error'
-
-class Question43(Page):
-    def is_displayed(self):
-        return self.session.config['treatment'] >= Constants.FirstOrder
-
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        temp = self.session.config['treatment']-1
-        return dict(
-            stage_number = temp,
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a43:
-            return 'error'
-#-----------------------------------------------------------------------------------
-
-
-
-#-----------------------------------------------------------------------------------
-# BLOCK 5 [SOB STAGE 3]:
-class Question51(Page):
-    def is_displayed(self):
-        return self.session.config['treatment'] >= Constants.DominantStrategy
-
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        temp = self.session.config['treatment']
-        return dict(
-            stage_number = temp,
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a51:
-            return 'error'
-
-
-
-class Question52(Page):
-    def is_displayed(self):
-        return self.session.config['treatment'] >= Constants.DominantStrategy
-
-    form_model  = 'player'
-    form_fields = ['question']
-
-    def vars_for_template(self):
-        temp = self.session.config['treatment']
-        return dict(
-            stage_number = temp,
-            )
-
-    form_show_errors = False
-
-    def before_next_page(self):
-        self.player.question_number = self.player.question_number+1
-
-    def error_message(player, values):
-        if values['question']!=Constants.a52:
-            return 'error'
-#-----------------------------------------------------------------------------------
-
-
 
 
 page_sequence = [
@@ -458,24 +152,8 @@ page_sequence = [
             Question1,
             Question2,
             Question3,
-            Question4,
-            Question5,
             # block 2:
+            Question4,
+            Question5, 
             Question6,
-            Question7,
-            Question8,
-            Question9,
-            Question10,
-            # block 3:
-#            Question31,
-#            Question32,
-#            Question33,
-#            Question34,
-            # block 4:
-#            Question41,
-#            Question42,
-#            Question43,
-            # block 5:
-#            Question51,
-#            Question52
             ]
